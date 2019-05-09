e15dd722a38349b48d47c70c80497ce622de4924
lizaigaoge550 <lizaigaoge550@gmail.com>
2019-05-09 09:38:17 +0800

    5.9commit

---
+++ tt.py
@@ -0,0 +1,29 @@
+import os
+import glob
+import pickle as pkl
+from main import extract_mention
+from pprint import pprint
+
+
+def printS():
+  count = 0
+  datas = pkl.load(open('data.pkl','rb'))
+  for data in datas:
+    if 'S' in data['tags'] and count < 10:
+      pprint(data)
+      count += 1
+
+
+
+
+if __name__ == '__main__':
+
+  printS()
+  # for file in glob.glob('valid_log/*'):
+  #   print(file)
+  #   data = pkl.load(open(file,'rb'))
+  #   #for i in range(len(data)):
+  #   #  data[i]['t_tag_mention'] = extract_mention(data[i]['t_tag'], data[i]['tokens'])
+  #   #  data[i]['p_tag_mention'] = extract_mention(data[i]['p_tag'], data[i]['tokens'])
+  #   #pkl.dump(data, open(file, 'wb'))
+  #   print(data)
